0eb7478da707aea688fc054e4658f544f2345636
unknown <qamar>
2025-11-26 22:39:02 +0500

    feat: add site url as output

--- main.tf
+++ main.tf
@@ -15,3 +15,10 @@ resource "aws_s3_object" "website_file" {
 }
 
 # ... (keep all your website configuration blocks the same)
+
+output "website_url" {
+  value = "http://${aws_s3_bucket.my_website_bucket.bucket}.s3-website-us-east-1.amazonaws.com"
+  
+  # This makes the URL show up nicely in GitHub Actions
+  description = "The URL of the live website"
+}
