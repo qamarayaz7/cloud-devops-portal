bf38ace0b2036e52c09491da5372e24e8ab5cc1f
unknown <qamar>
2025-11-26 22:18:19 +0500

    feat: add website URL output

--- main.tf
+++ main.tf
@@ -1,40 +1,47 @@
+# 1. CREATE THE BUCKET (THE EMPTY BOX)
 resource "aws_s3_bucket" "my_website_bucket" {
-  bucket_prefix = "qamar-devops-"  # Terraform will make it unique
+  bucket_prefix = "qamar-devops-"
 }
+
+# 2. MAKE IT A WEBSITE (TURN ON WEBSITE MODE)
 resource "aws_s3_bucket_website_configuration" "my_website" {
-  bucket = aws_s3_bucket.my_website_bucket.id
+  bucket = aws_s3_bucket.my_website_bucket.id  # <-- CONNECTS TO THE BUCKET
+
   index_document {
-    suffix = "index.html"
+    suffix = "index.html"  # <-- TELLS AWS: "SHOW index.html AS HOME PAGE"
   }
 }
 
-# ADD THIS BLOCK - it allows public access
+# 3. UNLOCK THE DOOR (ALLOW PUBLIC ACCESS)
 resource "aws_s3_bucket_public_access_block" "my_website" {
-  bucket = aws_s3_bucket.my_website_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  bucket = aws_s3_bucket.my_website_bucket.id  # <-- CONNECTS TO THE BUCKET
+
+  block_public_acls       = false  # <-- "DON'T BLOCK PUBLIC ACCESS"
+  block_public_policy     = false  # <-- "DON'T BLOCK PUBLIC POLICIES"
+  ignore_public_acls      = false  # <-- "DON'T IGNORE PUBLIC REQUESTS"
+  restrict_public_buckets = false  # <-- "DON'T RESTRICT PUBLIC BUCKETS"
 }
 
+# 4. PUT UP THE WELCOME SIGN (ALLOW EVERYONE TO READ)
 resource "aws_s3_bucket_policy" "allow_public_read" {
-  bucket = aws_s3_bucket.my_website_bucket.id
+  bucket = aws_s3_bucket.my_website_bucket.id  # <-- CONNECTS TO THE BUCKET
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
+        Effect    = "Allow"        # <-- "ALLOW PEOPLE TO..."
+        Principal = "*"            # <-- "EVERYONE"
+        Action    = "s3:GetObject" # <-- "VIEW FILES"
         Resource = [
-          aws_s3_bucket.my_website_bucket.arn,
-          "${aws_s3_bucket.my_website_bucket.arn}/*",
+          "${aws_s3_bucket.my_website_bucket.arn}/*",  # <-- "IN THIS BUCKET"
         ]
       },
     ]
   })
 }
+
+# 5. GET THE ADDRESS (SHOW YOU THE WEBSITE URL)
 output "website_url" {
   value = "http://${aws_s3_bucket.my_website_bucket.bucket}.s3-website-us-east-1.amazonaws.com"
 }
