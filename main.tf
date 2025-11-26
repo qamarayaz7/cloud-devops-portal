258258b39092414c1e875cc323495f8ed70e6927
unknown <qamar>
2025-11-26 22:21:59 +0500

    fix: complete website configuration

--- main.tf
+++ main.tf
@@ -1,47 +1,71 @@
-# 1. CREATE THE BUCKET (THE EMPTY BOX)
+# Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create S3 bucket with unique name
 resource "aws_s3_bucket" "my_website_bucket" {
   bucket_prefix = "qamar-devops-"
 }
 
-# 2. MAKE IT A WEBSITE (TURN ON WEBSITE MODE)
+# Upload website file to the bucket
+resource "aws_s3_object" "website_file" {
+  bucket = aws_s3_bucket.my_website_bucket.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+# Configure the bucket as a website
 resource "aws_s3_bucket_website_configuration" "my_website" {
-  bucket = aws_s3_bucket.my_website_bucket.id  # <-- CONNECTS TO THE BUCKET
+  bucket = aws_s3_bucket.my_website_bucket.id
 
   index_document {
-    suffix = "index.html"  # <-- TELLS AWS: "SHOW index.html AS HOME PAGE"
+    suffix = "index.html"
   }
 }
 
-# 3. UNLOCK THE DOOR (ALLOW PUBLIC ACCESS)
+# Allow public access to the bucket
 resource "aws_s3_bucket_public_access_block" "my_website" {
-  bucket = aws_s3_bucket.my_website_bucket.id  # <-- CONNECTS TO THE BUCKET
+  bucket = aws_s3_bucket.my_website_bucket.id
 
-  block_public_acls       = false  # <-- "DON'T BLOCK PUBLIC ACCESS"
-  block_public_policy     = false  # <-- "DON'T BLOCK PUBLIC POLICIES"
-  ignore_public_acls      = false  # <-- "DON'T IGNORE PUBLIC REQUESTS"
-  restrict_public_buckets = false  # <-- "DON'T RESTRICT PUBLIC BUCKETS"
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-# 4. PUT UP THE WELCOME SIGN (ALLOW EVERYONE TO READ)
+# Create policy to allow public read access
 resource "aws_s3_bucket_policy" "allow_public_read" {
-  bucket = aws_s3_bucket.my_website_bucket.id  # <-- CONNECTS TO THE BUCKET
+  bucket = aws_s3_bucket.my_website_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Sid       = "PublicReadGetObject"
-        Effect    = "Allow"        # <-- "ALLOW PEOPLE TO..."
-        Principal = "*"            # <-- "EVERYONE"
-        Action    = "s3:GetObject" # <-- "VIEW FILES"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
         Resource = [
-          "${aws_s3_bucket.my_website_bucket.arn}/*",  # <-- "IN THIS BUCKET"
+          "${aws_s3_bucket.my_website_bucket.arn}/*",
         ]
       },
     ]
   })
+
+  depends_on = [aws_s3_bucket_public_access_block.my_website]
 }
 
-# 5. GET THE ADDRESS (SHOW YOU THE WEBSITE URL)
+# Output the website URL
 output "website_url" {
   value = "http://${aws_s3_bucket.my_website_bucket.bucket}.s3-website-us-east-1.amazonaws.com"
 }
