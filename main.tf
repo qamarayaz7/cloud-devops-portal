6cb0f02bd58a24524d75293907c80d06520e253c
unknown <qamar>
2025-11-28 00:11:11 +0500

    add cloudfront for global content delivery

--- main.tf
+++ main.tf
@@ -49,6 +49,48 @@ resource "aws_s3_bucket_policy" "allow_public_read" {
   })
 }
 
+# CloudFront CDN for global content delivery
+resource "aws_cloudfront_distribution" "website_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.my_website_bucket.bucket_regional_domain_name
+    origin_id   = "S3-Website"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-Website"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# Update the output to show CloudFront URL instead of S3
 output "website_url" {
-  value = "http://${aws_s3_bucket.my_website_bucket.bucket}.s3-website-us-east-1.amazonaws.com"
+  value = "https://${aws_cloudfront_distribution.website_cdn.domain_name}"
 }
